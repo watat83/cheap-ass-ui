6ae1c0d781b7dced3210c61baca5fe278f61e0c4
anshuopinion <anshusopinion@gmail.com>
2023-07-23 17:33:54 +0530

    my-listing done

--- src/components/HomeLink/index.tsx
+++ src/components/HomeLink/index.tsx
@@ -1,6 +1,6 @@
 import { ROUTES } from "@/routes";
 import { Link } from "@chakra-ui/next-js";
-import { Text } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import { FC } from "react";
 
 interface IHomeLinkProps {}
@@ -8,9 +8,9 @@ interface IHomeLinkProps {}
 const HomeLink: FC<IHomeLinkProps> = ({}) => {
   return (
     <Link href={ROUTES.HOME}>
-      <Text textStyle="p" fontWeight="normal">
+      <Button size="sm" variant="white">
         Home
-      </Text>
+      </Button>
     </Link>
   );
 };
