5954f2bae3d4c2ed8578b25b8092a687d9fe2181
anshuopinion <anshusopinion@gmail.com>
2023-07-23 18:15:05 +0530

    edit address done

--- src/pages/edit-address.tsx
+++ src/pages/edit-address.tsx
@@ -6,13 +6,9 @@ import {
   FormControl,
   FormErrorMessage,
   HStack,
-  Input,
-  InputGroup,
-  InputRightElement,
   Stack,
   Text,
 } from "@chakra-ui/react";
-import { SlCalender } from "react-icons/sl";
 import Select from "react-select";
 import { Field, FieldProps, Form, Formik } from "formik";
 import * as Yup from "yup";
@@ -21,11 +17,12 @@ import Navbar from "@/components/Navbar";
 import FormInputWrappper from "@/components/FormInputWrapper/FormInputWrapper";
 import { platforms, tipBaitOptions } from "@/constant";
 import BottomLayout from "@/components/Layout/BottomLayout";
+import HomeLink from "@/components/HomeLink";
 
 const styles = {
   control: (provided: any, state: any) => ({
     ...provided,
-    borderRadius: "999px",
+    borderRadius: "10px",
     height: "60px",
     padding: "0 20px",
     backgroundColor: "#212121",
@@ -55,16 +52,15 @@ const styles = {
 };
 
 const validationSchema = Yup.object({
-  date: Yup.date().required("Please enter a date"),
   address: Yup.string().required("Please enter an address"),
   platform: Yup.object({
-    value: Yup.string().required("Please select a platform"),
+    value: Yup.string().max(20).required("Please select a platform"),
     label: Yup.string().required("Please select a platform"),
   }),
-  tip: Yup.number().required("Please enter a tip amount"),
-  endTip: Yup.number().required("Please enter an end tip amount"),
-  note: Yup.string().required("Please enter a note"),
-  apt: Yup.string().required("Please enter an Apt #"),
+  tip: Yup.string(),
+  endTip: Yup.number(),
+  note: Yup.string(),
+  apt: Yup.string(),
   tipBait: Yup.object({
     value: Yup.string().required("Please select a tip bait"),
     label: Yup.string().required("Please select a tip bait"),
@@ -77,13 +73,12 @@ const EditAddress = () => {
   return (
     <Formik
       initialValues={{
-        date: "",
         address: "",
         platform: {
           value: platforms[0].value,
           label: platforms[0].label,
         },
-        tip: "",
+        tip: "N/A",
         endTip: "",
         note: "",
         tipBaitsOther: "",
@@ -102,43 +97,6 @@ const EditAddress = () => {
           <Navbar pageName="Edit Address" toggleRightCollapse={toggleSidebar} />
           <Flex direction="column" justify="space-between" mb="32">
             <Stack px={4} spacing={5}>
-              <Box borderRadius="full" p={2} w="full">
-                <Text textAlign="center" textStyle="p">
-                  Post address only.
-                </Text>
-                <Text textAlign="center" textStyle="p">
-                  Please do not list name of any individuals.
-                </Text>
-              </Box>
-
-              <HStack alignItems="start" mt="4">
-                <Field name="date">
-                  {({ field, meta }: FieldProps) => (
-                    <FormControl isInvalid={!!(meta.error && meta.touched)}>
-                      <InputGroup position="relative" w="70%">
-                        <Input
-                          variant="filled"
-                          type="date"
-                          placeholder="Input date"
-                          {...field}
-                          pr="4"
-                          cursor="pointer"
-                        />
-                        <InputRightElement
-                          position="absolute"
-                          top={0}
-                          pointerEvents="none"
-                          right={0}
-                        >
-                          <SlCalender color="white" />
-                        </InputRightElement>
-                      </InputGroup>
-                      <FormErrorMessage>{meta.error}</FormErrorMessage>
-                    </FormControl>
-                  )}
-                </Field>
-                <Box w="40%"></Box>
-              </HStack>
               <HStack alignItems="start">
                 <FormInputWrappper
                   type="input"
@@ -151,83 +109,88 @@ const EditAddress = () => {
                   }}
                   inputProps={{
                     pl: 24,
-                    placeholder: "123 Main St, O'Fallon Mo",
+                    placeholder: "123 Main St, O’fallon MO",
                   }}
                 />
                 <Box w="40%">
                   <FormInputWrappper
                     type="input"
                     name="apt"
-                    leftElement={"#"}
-                    leftElementProps={{
-                      fontSize: "15px",
-                      fontWeight: "semibold",
-                      w: "40px",
-                    }}
                     inputProps={{
-                      pl: 10,
                       placeholder: "Apt #",
                     }}
                   />
                 </Box>
               </HStack>
-
-              <Field name="platform">
-                {({ field, meta }: FieldProps) => (
-                  <FormControl isInvalid={!!(meta.error && meta.touched)}>
-                    <Select
-                      id="platform"
-                      placeholder="Platform"
-                      options={platforms}
-                      styles={styles}
-                      {...field}
-                      onChange={(value) => setFieldValue("platform", value)}
+              <Flex gap="2">
+                <Stack flex="1">
+                  <Field name="platform">
+                    {({ field, meta }: FieldProps) => (
+                      <FormControl isInvalid={!!(meta.error && meta.touched)}>
+                        <Select
+                          id="platform"
+                          placeholder="Platform"
+                          options={platforms}
+                          styles={styles}
+                          {...field}
+                          onChange={(value) => setFieldValue("platform", value)}
+                        />
+                        <FormErrorMessage>{meta.error}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  {values.platform.label === "Other" && (
+                    <FormInputWrappper
+                      type="input"
+                      name="platform.value"
+                      inputProps={{
+                        placeholder:
+                          "Other (Please specify in notes section below)",
+                      }}
                     />
-                    <FormErrorMessage>{meta.error}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              {values.platform.label === "Other" && (
-                <FormInputWrappper
-                  type="input"
-                  name="platform.value"
-                  inputProps={{
-                    placeholder:
-                      "Other (Please specify in notes section below)",
-                  }}
-                />
-              )}
-              <Field name="tipBait">
-                {({ field, meta }: FieldProps) => (
-                  <FormControl isInvalid={!!(meta.error && meta.touched)}>
-                    <Select
-                      id="tipBait"
-                      placeholder="Tip Bait"
-                      options={tipBaitOptions}
-                      styles={styles}
-                      {...field}
-                      onChange={(value) => setFieldValue("tipBait", value)}
+                  )}
+                </Stack>
+                <Stack flex="1">
+                  <Field name="tipBait">
+                    {({ field, meta }: FieldProps) => (
+                      <FormControl isInvalid={!!(meta.error && meta.touched)}>
+                        <Select
+                          id="tipBait"
+                          placeholder="Tip Bait"
+                          options={tipBaitOptions}
+                          styles={styles}
+                          {...field}
+                          onChange={(value) => setFieldValue("tipBait", value)}
+                        />
+                        <FormErrorMessage>{meta.error}</FormErrorMessage>
+                      </FormControl>
+                    )}
+                  </Field>
+                  {values.tipBait.label === "Other" && (
+                    <FormInputWrappper
+                      type="input"
+                      name="tipBait.value"
+                      inputProps={{
+                        placeholder:
+                          "Other (Please specify in notes section below)",
+                      }}
                     />
-                    <FormErrorMessage>{meta.error}</FormErrorMessage>
-                  </FormControl>
-                )}
-              </Field>
-              {values.tipBait.label === "Other" && (
-                <FormInputWrappper
-                  type="input"
-                  name="tipBait.value"
-                  inputProps={{
-                    placeholder:
-                      "Other (Please specify in notes section below)",
-                  }}
-                />
-              )}
+                  )}
+                </Stack>
+              </Flex>
               <Flex gap={2}>
                 <Stack>
                   <Text textStyle="h5">Tip</Text>
                   <FormInputWrappper
                     type="input"
                     name="tip"
+                    isDisabled={
+                      !(
+                        values.tipBait.value === tipBaitOptions[0].value ||
+                        values.tipBait.value === tipBaitOptions[1].value ||
+                        values.tipBait.value === tipBaitOptions[2].value
+                      )
+                    }
                     leftElement={"$"}
                   />
                 </Stack>
@@ -241,6 +204,12 @@ const EditAddress = () => {
                   <FormInputWrappper
                     type="input"
                     name="endTip"
+                    isDisabled={
+                      !(
+                        values.tipBait.value === tipBaitOptions[0].value ||
+                        values.tipBait.value === tipBaitOptions[1].value
+                      )
+                    }
                     leftElement={"$"}
                   />
                 </Stack>
@@ -249,12 +218,15 @@ const EditAddress = () => {
                 type="textarea"
                 label="Notes"
                 name="note"
+                placeholder="Do not list any names of individuals"
                 leftElement={"$"}
               />
 
-              <BottomLayout justify="end">
+              <BottomLayout>
+                <HomeLink />
+
                 <Button w="114px" h="40px" bg="brand.red" type="submit">
-                  SAVE
+                  Post
                 </Button>
               </BottomLayout>
             </Stack>
