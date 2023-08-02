d53fa1272c3458300a29aa01e87ce4a590296043
anshuopinion <anshusopinion@gmail.com>
2023-08-02 07:50:45 +0530

    added splash screen

--- src/containers/Splash/index.tsx
+++ src/containers/Splash/index.tsx
@@ -1,20 +1,30 @@
 import BackgroundLayout from "@/components/Layout/BackgroundLayout/BackgroundLayout";
-import { Grid, Image, VStack } from "@chakra-ui/react";
+import { Flex, Grid, Image, VStack } from "@chakra-ui/react";
 
 const Splash = () => {
   return (
     <>
-      <BackgroundLayout>
-        <Grid placeItems="center" mt="250px" w="full">
-          <VStack spacing={0}>
-            <Image
-              boxSize="150px"
-              src="/assets/images/donkey_logo.webp"
-              borderRadius="50%"
-            />
-          </VStack>
-        </Grid>
-      </BackgroundLayout>
+      <Flex
+        direction="column"
+        minH="100vh"
+        w="100%"
+        bg="brand.dark"
+        color="brand.white"
+        maxW="md"
+        mx="auto"
+      >
+        <BackgroundLayout>
+          <Grid placeItems="center" mt="250px" w="full">
+            <VStack spacing={0}>
+              <Image
+                boxSize="150px"
+                src="/assets/images/donkey_logo.webp"
+                borderRadius="50%"
+              />
+            </VStack>
+          </Grid>
+        </BackgroundLayout>
+      </Flex>
     </>
   );
 };
